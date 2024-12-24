662570328c9cde557413026a1446807ddb03f7e2
julian <urerc@student.kit.edu>
2024-12-24 22:33:51 +0100

    ruff linting

--- control.py
+++ control.py
@@ -2,7 +2,6 @@ import yaml
 from mimo_buffer import mimoBuffer
 from mimo_worker import mimoWorker
 import numpy as np
-import multiprocessing
 import os
 from graphviz import Digraph
 import time
@@ -11,58 +10,50 @@ import shutil
 
 class mimoControl:
     def __init__(self, buffers_setup: dict, functions_setup: dict, function_configs: dict):
-        
         self.buffers_setup = buffers_setup
         self.functions_setup = functions_setup
         self.function_configs = function_configs
-        
-            
+
     def initialize_buffers(self):
         self.buffers_dict = {}
         for name, setup in self.buffers_setup_dict.items():
             self.buffers_dict[name] = mimoBuffer(
-                name = name,
-                slot_count = setup['slot_count'],
-                data_length = setup['data_length'],
-                data_dtype = np.dtype(setup['data_dtype']),
-                overwrite = setup['overwrite']
-                )
-            
+                name=name,
+                slot_count=setup['slot_count'],
+                data_length=setup['data_length'],
+                data_dtype=np.dtype(setup['data_dtype']),
+                overwrite=setup['overwrite'],
+            )
+
     def initialize_functions(self):
         self.functions_dict = {}
         for name in self.functions_setup.items():
             setup = self.functions_setup[name]
             config = self.functions_config[name]
-            
+
             self.functions_dict[name] = mimoWorker(
-                name = name,
-                function = setup['function'],
-                args = (
+                name=name,
+                function=setup['function'],
+                args=(
                     self._get_buffers_from_strings(setup['source_list']),
                     self._get_buffers_from_strings(setup['sink_list']),
                     self._get_buffers_from_strings(setup['observe_list']),
                     config,
                 ),
-                number_of_processes = setup['number_of_processes']
+                number_of_processes=setup['number_of_processes'],
             )
-            
+
     def _get_buffers_from_strings(self, strings: list[str]):
         [self.buffers_dict[name] for name in strings]
-            
+
     def start_functions(self):
         for function in self.functions_dict.values():
             function.initialize_processes()
         for function in self.functions_dict.values():
             function.start_processes()
-            
-            
 
-        
     def check_data_flow(self) -> bool:
-        """Checks the data flow
-        
-        
-        """
+        """Checks the data flow"""
         # re-sort the setup, i.e. for each buffer a list of reader, writer and observer functions
         writers = {buffer_name: [] for buffer_name in self.buffers_setup.keys()}
         readers = {buffer_name: [] for buffer_name in self.buffers_setup.keys()}
@@ -74,15 +65,14 @@ class mimoControl:
                 readers[buffer_name].append(function_name)
             for buffer_name in setup['observe_list']:
                 observers[buffer_name].append(function_name)
-            
+
         # check that each buffer has at most one writer
         for buffer_name, writer_list in writers.items():
             if len(writer_list) > 1:
                 return False
 
-            
         # check that there is only one root buffer and every other buffer is reachable from there
-        
+
         # -> find all candidates for producer functions
         candidate_producer_functions = []
         for function_name, setup in self.functions_setup.items():
@@ -92,33 +82,32 @@ class mimoControl:
         if len(candidate_producer_functions) != 1:
             return False
         producer_function = candidate_producer_functions[0]
-        
+
         # -> check that the producer function has only one sink buffer
         root_buffers = self.functions_setup[producer_function]['sink_list']
         if len(root_buffers) != 1:
             return False
         root_buffer = root_buffers[0]
-        
+
         # -> check that all other buffers are reachable from the root buffer
         reachable = set([root_buffer])
         while True:
             previous = reachable.copy()
-            
+
             for buffer_name in reachable:
                 # add all sinks of the functions, that read from this buffer to the reachables
                 for function_name in readers[buffer_name]:
                     reachable.update(self.functions_setup[function_name]['sink_list'])
-                    
+
             if previous == reachable:
                 break
-            
+
         if set(self.buffers_setup.keys()) != reachable:
             return False
-    
 
         # now every buffer has at most one writer and every buffer is reachable from the root buffer => data flow is an arborescence
         return True
-    
+
     def visualize_buffers_and_functions(self, **kwargs):
         dot = Digraph(**kwargs)
         for buffer_name in self.buffers_setup.keys():
@@ -135,53 +124,50 @@ class mimoControl:
         dot.render('data_flow', cleanup=True)
         dot.view()
         # TODO how do i want to return the visualization?
-       
+
     def visualize_arboresence(self, **kwargs):
         """TODO visualize only a view of the ringbuffers with the functions in between"""
         raise NotImplementedError
-    
-    
+
+
 class fileReader:
     def __init__(self, setup_file):
         self.setup_file = os.path.abspath(setup_file)
         self.setup_dir = os.path.dirname(self.setup_file)
-        
+
         self.loaded_config_files = {}
-    
+
     def __call__(self):
         self.load_setup()
         self.create_buffers_setup()
         self.create_functions_setup()
         self.create_config_dicts()
         return self.buffers_setup, self.functions_setup, self.function_configs
-        
+
     def load_setup(self):
         # load the setup file
         with open(self.setup_file, 'r') as stream:
             setup = yaml.safe_load(stream)
-        
+
         # read the setup
         self.options = setup.get('Options', {})
         self.buffers = setup.get('Buffers', {})
         self.functions = setup.get('Functions', {})
-        
+
         # create the target directory
         self.output_directory = os.path.join(self.setup_dir, self.options.get('output_directory', 'target'))
         os.makedirs(self.output_directory, exist_ok=True)
-        
+
         # create the run directory
         start_time = time.strftime('%Y-%m-%d_%H-%M-%S')
         self.run_directory = os.path.join(
-            self.output_directory,
-            self.options.get('run_directory', 'run') + '_' + start_time
-            )
+            self.output_directory, self.options.get('run_directory', 'run') + '_' + start_time
+        )
         os.makedirs(self.run_directory, exist_ok=False)
-        
+
         # copy the setup file to the run directory
         shutil.copy(self.setup_file, self.run_directory)
-        
 
-        
     def load_config_file(self, file: str):
         file = os.path.join(self.input_path, file)
         if file in self.loaded_config_files:
@@ -191,20 +177,20 @@ class fileReader:
             config = yaml.safe_load(stream)
         self.loaded_config_files[file] = config
         return config
-        
+
     def create_buffers_setup(self):
         # main overwrite
         overwrite = self.options.get('overwrite', True)
-        
+
         self.buffers_setup = {}
         for name, setup in self.buffers.items():
             self.buffers_setup[name] = {
                 'slot_count': setup.get('slot_count', 1),
                 'data_length': setup.get('data_length', 1),
                 'data_dtype': setup.get('data_dtype', 'float64'),
-                'overwrite': setup.get('overwrite', overwrite)
+                'overwrite': setup.get('overwrite', overwrite),
             }
-            
+
     def create_functions_setup(self):
         self.functions_setup = {}
         for name, setup in self.functions.items():
@@ -213,18 +199,18 @@ class fileReader:
                 'source_list': setup.get('source_list', []),
                 'sink_list': setup.get('sink_list', []),
                 'observe_list': setup.get('observe_list', []),
-                'number_of_processes': setup.get('number_of_processes', 1)
+                'number_of_processes': setup.get('number_of_processes', 1),
             }
-            
+
     def create_config_dicts(self):
         # overarching config dict
         if 'overarching_config' in self.options:
             overarching_config = self.load_config_file(self.options['overarching_config'])
         else:
             overarching_config = {}
-        
+
         self.function_configs = {key: overarching_config for key in self.functions_setup.keys()}
-        
+
         # main config file
         if 'main_config' in self.options:
             main_config = self.load_config_file(self.options['main_config'])
@@ -233,22 +219,22 @@ class fileReader:
         for function_name in self.functions_setup.keys():
             if function_name in main_config:
                 self.function_configs[function_name].update(main_config[function_name])
-                
+
         # individual config files
         for function_name, setup in self.functions_setup.items():
             if 'config' in setup:
                 self.function_configs[function_name].update(self.load_config_file(setup['config']))
-                
+
         # obligatory config
         OBLIGATORY_KEYS = ['run_directory', 'name']
         for function_name in self.functions_setup.keys():
             overwriting_keys = [key for key in OBLIGATORY_KEYS if key in self.function_configs[function_name]]
             if overwriting_keys:
                 raise RuntimeWarning(f"Overwriting keys {overwriting_keys} in config of {function_name}")
-            
-            self.function_configs[function_name].update({
-                'run_directory': self.run_directory,
-                'name': function_name,
-            })
-                
-                
+
+            self.function_configs[function_name].update(
+                {
+                    'run_directory': self.run_directory,
+                    'name': function_name,
+                }
+            )
--- mimo_worker.py
+++ mimo_worker.py
@@ -1,31 +1,26 @@
 import multiprocessing
 
+
 class mimoWorker:
     def __init__(self, name: str, function, args, number_of_processes: int):
         self.name = name
         self.function = function
-        self.args = args   
+        self.args = args
         self.number_of_processes = number_of_processes
-             
+
         self.processes = []
-        
-        
+
     def initialize_processes(self):
         if len(self.processes) > 0:
             raise RuntimeError("Processes already initialized")
         for i in range(self.number_of_processes):
-            process = multiprocessing.Process(
-                target=self.function,
-                args=self.args,
-                name=f'{self.name}_{i}'
-            )
+            process = multiprocessing.Process(target=self.function, args=self.args, name=f'{self.name}_{i}')
             self.processes.append(process)
 
-            
     def start_processes(self):
         for process in self.processes:
             process.start()
-            
+
     def shutdown(self):
         for p in self.processes:
             if p.is_alive():
