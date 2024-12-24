e1559ea0f7d6a955d26c7af10b980b71caf99fe6
julian <urerc@student.kit.edu>
2024-12-24 14:39:15 +0100

    check the data flow if it is an arborescence

--- control.py
+++ control.py
@@ -51,12 +51,75 @@ class mimoControl:
             function.start_processes()
             
             
-    def visualize_data_flow(self, **kwargs):
+
+        
+    def check_data_flow(self) -> bool:
+        """Checks the data flow
+        
+        
+        """
+        # resort the setup, i.e. for each buffer a list of reader, writer and observer functions
+        writers = {buffer_name: [] for buffer_name in self.buffers_setup.keys()}
+        readers = {buffer_name: [] for buffer_name in self.buffers_setup.keys()}
+        observers = {buffer_name: [] for buffer_name in self.buffers_setup.keys()}
+        for function_name, setup in self.functions_setup.items():
+            for buffer_name in setup['sink_list']:
+                writers[buffer_name].append(function_name)
+            for buffer_name in setup['source_list']:
+                readers[buffer_name].append(function_name)
+            for buffer_name in setup['observe_list']:
+                observers[buffer_name].append(function_name)
+            
+        # check that each buffer has at most one writer
+        for buffer_name, writer_list in writers.items():
+            if len(writer_list) > 1:
+                return False
+
+            
+        # check that there is only one root buffer and every other buffer is reachable from there
+        
+        # -> find all candidates for producer functions
+        candidate_producer_functions = []
+        for function_name, setup in self.functions_setup.items():
+            if len(setup['source_list']) == 0 and len(setup['observe_list']) == 0 and len(setup['sink_list']) > 0:
+                candidate_producer_functions.append(function_name)
+        # -> check that only one producer function exists
+        if len(candidate_producer_functions) != 1:
+            return False
+        producer_function = candidate_producer_functions[0]
+        
+        # -> check that the producer function has only one sink buffer
+        root_buffers = self.functions_setup[producer_function]['sink_list']
+        if len(root_buffers) != 1:
+            return False
+        root_buffer = root_buffers[0]
+        
+        # -> check that all other buffers are reachable from the root buffer
+        reachable = set([root_buffer])
+        while True:
+            previous = reachable.copy()
+            
+            for buffer_name in reachable:
+                # add all sinks of the functions, that read from this buffer to the reachables
+                for function_name in readers[buffer_name]:
+                    reachable.update(self.functions_setup[function_name]['sink_list'])
+                    
+            if previous == reachable:
+                break
+            
+        if set(self.buffers_setup.keys()) != reachable:
+            return False
+    
+
+        # now every buffer has at most one writer and every buffer is reachable from the root buffer => data flow is an arborescence
+        return True
+    
+    def visualize_buffers_and_functions(self, **kwargs):
         dot = Digraph(**kwargs)
         for buffer_name in self.buffers_setup.keys():
-            dot.node('B' + buffer_name)
+            dot.node('B' + buffer_name, shape='circle', label=buffer_name)
         for function_name in self.functions_setup.keys():
-            dot.node('F' + function_name)
+            dot.node('F' + function_name, shape='box', label=function_name)
         for function_name, setup in self.functions_setup.items():
             for source in setup['source_list']:
                 dot.edge('B' + source, 'F' + function_name)
@@ -67,4 +130,7 @@ class mimoControl:
         dot.render('data_flow', cleanup=True)
         dot.view()
         # TODO how do i want to return the visualization?
-        
+       
+    def visualize_arboresence(self, **kwargs):
+        """TODO visualize only a view of the ringbuffers with the functions in between"""
+        raise NotImplementedError
