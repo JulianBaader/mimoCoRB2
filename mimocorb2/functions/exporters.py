3ace29a5db14faeace46c507b8d292ebb42d2fa2
julian <urerc@student.kit.edu>
2025-01-15 18:33:26 +0100

    add a visualisation to the histogram

--- mimocorb2/functions/exporters.py
+++ mimocorb2/functions/exporters.py
@@ -1,8 +1,10 @@
 from mimocorb2.function_templates import Exporter
 
 import numpy as np
+import multiprocessing
 import time
 import os
+import matplotlib.pyplot as plt
 
 def drain(*mimo_args):
     exporter = Exporter(mimo_args)
@@ -25,6 +27,7 @@ def histogram(*mimo_args):
     run_directory = exporter.config['run_directory']
     update_interval = exporter.config.get('update_interval', 1)
     bin_config = exporter.config['bins']
+    visualize = exporter.config.get('visualize', False)
     requested_channels = bin_config.keys()
     for rch in requested_channels:
         if rch not in available_channels:
@@ -38,9 +41,18 @@ def histogram(*mimo_args):
         run_directory = os.path.join(run_directory, name)
         os.makedirs(run_directory, exist_ok=True)
     for ch in channels:
-        files[ch] = os.path.join(run_directory, name + '_' + ch + '.hst')
+        files[ch] = os.path.join(run_directory, name + '_' + ch + '.npy')
         bins[ch] = np.linspace(bin_config[ch][0], bin_config[ch][1], bin_config[ch][2])
         hists[ch] = np.histogram([], bins=bins[ch])[0]
+    
+    def save_hists():
+        for ch in channels:
+            np.save(files[ch], hists[ch])
+    save_hists()
+    
+    if visualize:
+        p = multiprocessing.Process(target=sub_histogram, args=(files, bins, update_interval, name))
+        p.start()
     
     generator = exporter()
     last_save = time.time()
@@ -53,8 +65,40 @@ def histogram(*mimo_args):
             hists[ch] += np.histogram(data[ch], bins=bins[ch])[0]
         
         if time.time() - last_save > update_interval:
-            for ch in channels:
-                np.save(files[ch], hists[ch])
+            save_hists()
             last_save = time.time()
-    for ch in channels:
-        np.save(files[ch], hists[ch])
+    save_hists()
+    if visualize:
+        p.terminate()
+        
+def sub_histogram(files, bins, update_interval, name):
+    channels = list(files.keys())
+    fig = plt.figure()
+    fig.canvas.manager.set_window_title('Histogram ' + name)
+    
+    n_plots = len(channels)
+    cols = int(np.ceil(np.sqrt(n_plots)))
+    rows = int(np.ceil(n_plots / cols))
+    axes = fig.subplots(rows, cols)
+    axes = axes.flatten()
+    
+    lines = {ch: ax.plot(bins[ch][:-1], np.load(files[ch]))[0] for ch, ax in zip(channels, axes)}
+    
+    plt.ion()
+    plt.show()
+    
+    last_update = time.time()
+    while True:
+        if time.time() - last_update > update_interval:
+            for i, ch in enumerate(channels):
+                try:
+                    lines[ch].set_ydata(np.load(files[ch]))
+                except EOFError:
+                    continue
+                axes[i].relim()
+                axes[i].autoscale_view()
+            fig.canvas.draw()
+        fig.canvas.flush_events()
+        time.sleep(1/20)
+    
+    # TODO why dont i count frames?
