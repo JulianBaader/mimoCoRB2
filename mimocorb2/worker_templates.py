0b7a2157cedf8ae23ed9626005ede017a70ecdcb
julian <urerc@student.kit.edu>
2025-02-15 19:34:13 +0100

    improve documentation of worker_templates

--- mimocorb2/worker_templates.py
+++ mimocorb2/worker_templates.py
@@ -3,8 +3,9 @@ import time
 import os
 import numpy as np
 from typing import TypeAlias, Callable, Generator
+from mimocorb2.mimo_buffer import BufferReader, BufferWriter, BufferObserver
 
-ArgsAlias: TypeAlias = list[list, list, list, dict]
+ArgsAlias: TypeAlias = list[list[BufferReader], list[BufferWriter], list[BufferObserver], dict]
 
 METADATA = 0
 DATA = 1
@@ -52,23 +53,38 @@ class Template:
 
 
 class Importer(Template):
-    """Importer class for importing data from an external generator.
+    """Worker class for importing data from an external generator.
 
+    Attributes
+    ----------
+    counter : int
+        Counter for the number of events imported
+    writer : BufferWriter
+        BufferWriter object for writing data to the buffer
+        
     Examples
     --------
     >>> def worker(*mimo_args):
     ...     importer = Importer(mimo_args)
     ...     config = importer.config
-    ...     buffer_name = importer.writer.name
     ...     data_example = importer.writer.data_example
+    ...     buffer_name = importer.writer.name
     ...     def ufunc():
-    ...         for i in range(config.get('n_iterations', 10)):
-    ...             yield np.random.rand(data_example.shape)
-    ...         yield None
-    ...     importer(ufunc)
+    ...        for i in range(config['n_events']):
+    ...            data = np.random.normal(size=data_example.shape)
+    ...            yield data
+    ...        yield None
+    ...     importer(ufunc)    
     """
 
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup.
+        
+        Parameters
+        ----------
+        mimo_args : ArgsAlias
+            List of sources, sinks, observes, and config dictionary
+        """
         super().__init__(mimo_args)
         self.counter = 0
 
@@ -82,6 +98,16 @@ class Importer(Template):
         self.writer = self.sinks[0]
 
     def __call__(self, ufunc: Callable) -> None:
+        """Start the generator and write data to the buffer.
+        
+        ufunc must yield data of the same format as the Importer.writer.data_example and yield None at the end.
+        Metadata (counter, timestamp, deadtime) is automatically added to the buffer.
+        
+        Parameters
+        ----------
+        ufunc : Callable
+            Generator function that yields data and ends with None    
+        """
         if not callable(ufunc):
             self.read_all.send_flush_event()
             raise RuntimeError("ufunc not callable")
@@ -119,7 +145,12 @@ class Importer(Template):
 
 
 class Exporter(Template):
-    """Exporter class for exporting.
+    """Worker class for exporting data and metadata.
+    
+    Attributes
+    ----------
+    reader : BufferReader
+        BufferReader object for reading data from the buffer
 
     Examples
     --------
@@ -136,6 +167,13 @@ class Exporter(Template):
     """
 
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup.
+        
+        Parameters
+        ----------
+        mimo_args : ArgsAlias
+            List of sources, sinks, observes, and config dictionary
+        """
         super().__init__(mimo_args)
 
         if len(self.sources) != 1:
@@ -148,6 +186,16 @@ class Exporter(Template):
         self.reader = self.sources[0]
 
     def __call__(self) -> Generator:
+        """Start the generator and yield data and metadata.
+        
+        Yields data and metadata from the buffer until the buffer is shutdown.
+        
+        Yields
+        ------
+        data : np.ndarray, None
+            Data from the buffer
+        metadata : np.ndarray, None
+            Metadata from the buffer"""
         while True:
             with self.reader as source:
                 data = source[DATA]
@@ -160,16 +208,36 @@ class Exporter(Template):
 
 
 class Filter(Template):
-    """Filter class for filtering data from one buffer to other buffer(s).
+    """Worker class for filtering data from one buffer to other buffer(s).
 
     Analyze data using ufunc(data) and copy or discard data based on the result.
-
-    ufunc(data) returns
-    bool -> Copy data and metadata to all buffers or discard data
-    list[bool] (mapping to sinks) -> copy data and metadata to the corresponding buffers or discard data
+    
+    Attributes
+    ----------
+    reader : BufferReader
+        BufferReader object for reading data from the buffer
+    writers : list[BufferWriter]
+        List of BufferWriter objects for writing data to the buffers
+        
+    Examples
+    --------
+    >>> def worker(*mimo_args):
+    ...     filter = Filter(mimo_args)
+    ...     min_height = filter.config['min_height']
+    ...     def ufunc(data):
+    ...         if np.max(data) > min_height:
+    ...             return True
+    ...         else:
+    ...             return False
+    ...     filter(ufunc)
     """
 
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup.
+        
+        Check that the number of sources, sinks, and observes are correct.
+        Check that the source and sink shapes and dtypes match.
+        """
         super().__init__(mimo_args)
 
         if len(self.sources) != 1:
@@ -191,6 +259,20 @@ class Filter(Template):
         self.writers = self.sinks
 
     def __call__(self, ufunc) -> None:
+        """Start the filter and copy or discard data based on the result of ufunc(data).
+        
+        Parameters
+        ----------
+        ufunc : Callable
+            Function which will be called upon the data (Filter.reader.data_example).
+            The function can return:
+                bool
+                    True: copy data to every sink
+                    False: discard data
+                list[bool] (mapping to the sinks)
+                    True: copy data to the corresponding sink
+                    False: dont copy data to the corresponding sink
+        """
         if not callable(ufunc):
             self.read_all.send_flush_event()
             raise RuntimeError("ufunc not callable")
@@ -223,14 +305,32 @@ class Filter(Template):
 
 
 class Processor(Template):
-    """Processor class for processing data from one buffer to other buffer(s).
+    """Worker class for processing data from one buffer to other buffer(s).
 
-    ufunc(data) returns
-    None -> Discard data
-    list[data, None] -> if none, dont write to that buffer, if data, write to that buffer
+    Attributes
+    ----------
+    reader : BufferReader
+        BufferReader object for reading data from the buffer
+    writers : list[BufferWriter]
+        List of BufferWriter objects for writing data to the buffers
+        
+    Examples
+    --------
+    >>> def worker(*mimo_args):
+    ...     processor = Processor(mimo_args)
+    ...     def ufunc(data):
+    ...         return [data + 1, data - 1]
+    ...     processor(ufunc)
     """
 
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup.
+        
+        Parameters
+        ----------
+        mimo_args : ArgsAlias
+            List of sources, sinks, observes, and config dictionary
+        """
         super().__init__(mimo_args)
 
         if len(self.sources) != 1:
@@ -244,6 +344,17 @@ class Processor(Template):
         self.writers = self.sinks
 
     def __call__(self, ufunc: Callable) -> None:
+        """Start the processor and process data using ufunc(data).
+        
+        Parameters
+        ----------
+        ufunc : Callable
+            Function which will be called upon the data (Processor.reader.data_example).
+            When the function returns None the data will be discarded.
+            Otherwise the function must return a list of results, one for each sink.
+            If the result is not None it will be written to the corresponding sink.
+        """
+            
         if not callable(ufunc):
             self.read_all.send_flush_event()
             raise RuntimeError("ufunc not callable")
@@ -271,7 +382,33 @@ class Processor(Template):
 
 
 class Observer(Template):
+    """Worker class for observing data from a buffer.
+    
+    Attributes
+    ----------
+    observer : BufferObserver
+        BufferObserver object for observing data from the buffer
+        
+    Examples
+    --------
+    >>> def worker(*mimo_args):
+    ...     observer = Observer(mimo_args)
+    ...     generator = observer()
+    ...     while True:
+    ...         data, metadata = next(generator)
+    ...         if data is None:
+    ...             break
+    ...         print(data, metadata)
+    ...         time.sleep(1)
+    """
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup.
+        
+        Parameters
+        ----------
+        mimo_args : ArgsAlias
+            List of sources, sinks, observes, and config dictionary
+        """
         super().__init__(mimo_args)
 
         if len(self.sources) != 0:
@@ -283,6 +420,17 @@ class Observer(Template):
         self.observer = self.observes[0]
 
     def __call__(self) -> Generator:
+        """Start the observer and yield data and metadata.
+        
+        Yields data and metadata from the buffer until the buffer is shutdown.
+        
+        Yields
+        ------
+        data : np.ndarray, None
+            Data from the buffer
+        metadata : np.ndarray, None
+            Metadata from the buffer
+        """
         while True:
             with self.observer as source:
                 data = source[DATA]
@@ -297,8 +445,19 @@ class Observer(Template):
 
 
 class Monitor(Template):
-    """Worker Class for Monitoring data transfered between two identical Buffers"""
+    """Worker Class for Monitoring data transfered between two identical Buffers.
+    
+    If no sink is provided, the Monitor will act as an Exporter.
+    
+    Attributes
+    ----------
+    reader : BufferReader
+        BufferReader object for reading data from the buffer
+    writer : BufferWriter
+        BufferWriter object for writing data to the buffer
+    """
     def __init__(self, mimo_args: ArgsAlias) -> None:
+        """Checks the setup."""
         super().__init__(mimo_args)
 
         if len(self.sources) != 1:
@@ -349,6 +508,17 @@ class Monitor(Template):
         yield None, None
         
     def __call__(self) -> Generator:
+        """Start the monitor and yield data and metadata.
+        
+        Yields data and metadata from the buffer until the buffer is shutdown.
+        
+        Yields
+        ------
+        data : np.ndarray, None
+            Data from the buffer
+        metadata : np.ndarray, None
+            Metadata from the buffer
+        """
         if self.writer is not None:
             self.logger.info("Starting Monitor as Monitor")
             return self._monitor()
