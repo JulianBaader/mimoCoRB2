d237247a532e8e36faadc6a2edc7538c37f58501
julian <urerc@student.kit.edu>
2025-06-16 15:37:41 +0200

    fix bug in redpitaya importer

--- mimocorb2/functions/importers/redpitaya.py
+++ mimocorb2/functions/importers/redpitaya.py
@@ -357,7 +357,7 @@ def waveform(buffer_io):
 
     rp.set_trigger_source(trigger_source)  # TODO
 
-    data_example = importer.data_example
+    data_example = importer.data_out_examples[0].copy()
 
     rp.set_total_number_of_samples(data_example.size)
 
